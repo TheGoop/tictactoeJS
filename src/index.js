0a73005edb1e06650698a061d31b23ff16cb6b4f
Akshay Gupta <akshaygupta.us@gmail.com>
2020-11-03 00:26:07 -0500

    made some of code for selecting a piece - missing portion of logic where they are allowed to pick a piece other than the middle piece (if they already control it) if their move is a winning move.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,40 @@ import './index.css';
     );
   }
   
+  function after3Moves(turn, xNext){
+      let player = "O"
+      if (xNext)
+      {
+          player = "X"
+      }
+    
+      if (player === "X")
+      {
+          //X starts on turn 0, then 2, then 4 so third move for X comes AFTER turn 4
+          if (turn > 4)
+          {
+              return true
+          }
+          else
+          {
+              return false
+          }
+      }
+      else
+      {
+          //if player is O
+          //O starts on turn 1, then 3, then 5, so third move for O comes AFTER turn 5
+          if (turn > 5)
+          {
+              return true
+          }
+          else
+          {
+              return false
+          }
+      }
+  }
+
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
@@ -37,6 +71,9 @@ import './index.css';
         this.state = {
             squares: Array(9).fill(null),
             xNext : true,
+            turn : 0,
+            select : false, 
+            selectedPiece : NaN,
         }
     }
 
@@ -44,13 +81,91 @@ import './index.css';
     {
         //create a copy of squares to modify
         const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
+
+
+        //if after 3 moves different behavior from normal
+        if (after3Moves(this.state.turn, this.state.xNext))
+        {
+            //if a piece has already been selected
+            if (this.state.select && isNaN(this.state.selectedPiece) === false)
+            {
+                
+            }
+
+            //else a piece has not already been selected by player
+            else{
+                //this means that this move must select a piece of his own to move
+
+                //figure out what player this is
+                let player = this.state.xNext ? 'X' : 'O'
+                //get index of middle position of board arry
+                let mid = Math.floor(this.state.squares.length / 2)
+
+                //if player has chosen to select his own piece
+                if (this.state.squares[i] === player)
+                {
+
+                    //if player doesn't control middle position
+                    if (this.state.squares[mid] !== player)
+                    {
+                        //since he doesn't have to pick the middle position,
+                        // and he has picked his own piece, he may select it
+                        this.setState({
+                            select:true,
+                            selectedPiece:i,
+                        })
+                    }
+
+                    //if the mid position is his, but he chosen to not select it
+                    else if (this.state.squares[mid] === player && mid !== i)
+                    {
+//MUST FIX HE CAN PLACE IF ITS A WINNING MOVE                        
+                        //he will have to try again at selecting
+                        this.setState({
+                            select:false,
+                            selectedPiece:NaN,
+                        });
+                        return
+                    }
+                    //else, the mid position is his and he chosen to selected it
+                    else
+                    {
+                        //he has selected the right position, and he will move it next turn
+                        this.setState({
+                            select:true,
+                            selectedPiece : mid,
+                        });
+                        return
+
+                    }
+                }
+                //he has chosen to select a piece that is either empty or not his 
+                else
+                {
+                    // cannot do this after 3 moves, therefore he must try again
+                    this.setState({
+                        select:false,
+                        selectedPiece:NaN,
+                    });
+                    return
+                }
+
+            }
+        }
+
+        //else if we 3 moves or before we have normal behavior 
+        //where one cannot replace occupied squares, or play if theres a winner
+        else if (calculateWinner(squares) || squares[i]) {
           return;
         }
+
+        //if 3 moves or before and we have a valid move, make it
+        //make move
         squares[i] = this.state.xNext ? 'X' : 'O';
         this.setState({
             squares: squares,
             xNext: !this.state.xNext,
+            turn: this.state.turn + 1,
         });
     }
 
@@ -69,7 +184,8 @@ import './index.css';
         if (winner) {
         status = 'Winner: ' + winner;
         } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            console.log(this.state.xNext)
+        status = 'Next player: ' + (this.state.xNext ? 'X' : 'O');
         }
 
       return (
